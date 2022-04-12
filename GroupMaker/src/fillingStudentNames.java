8f330bd54dbdd3c05f3f3546b57a48fa66299b4f
abigail.callahan <abigail.callahan@MU-C1-02.mullen.pvt.k12.co.us>
2022-04-12 13:47:09 -0600

    finished filling original arraylist

--- GroupMaker/src/fillingStudentNames.java
+++ GroupMaker/src/fillingStudentNames.java
@@ -4,23 +4,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 
-
 public class fillingStudentNames 
 	{
 		static Scanner file;
 		static ArrayList <String> studentNames = new ArrayList <String>();
-		
-		public static void fillStudentNames() throws IOException
+
+		public static void fillingStudentNames() throws IOException
 		{
 			file = new Scanner(new File("StudentList.txt"));
 			
 			while(file.hasNext())
 				{
 					String name = file.nextLine();
+					studentNames.add(name);
 				}
-			
 		}
-		
-
 		
 	}
